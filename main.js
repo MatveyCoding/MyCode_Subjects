15a1f6443f022e19823fe584355eb48c4e635c4c
MatveyCoding <korchemanov56@mail.ru>
2023-11-17 11:54:36 +0400

    All pages was created

--- main.js
+++ main.js
@@ -1,48 +1,8 @@
-let paragraph = document.querySelector('.unvisible_CBCD_paragraph')
-console.log(paragraph.style)
-function showParagraph()
+function showParagraphP(P)
 {
-if(paragraph.style.display==='none') 
-  paragraph.style.display = 'block';
+  let P_On = document.querySelector(P)
+ if(P_On.style.display==='none') 
+ P_On.style.display = 'block';
  else
-  paragraph.style.display = 'none';
-}
-
-let paragraph1 = document.querySelector('.unvisible_CBCD_paragraph1')
-console.log(paragraph.style)
-function showParagraph1()
-{
-if(paragraph1.style.display==='none') 
-  paragraph1.style.display = 'block';
- else
-  paragraph1.style.display = 'none';
-}
-
-let paragraph2 = document.querySelector('.unvisible_CBCD_paragraph2')
-console.log(paragraph.style)
-function showParagraph2()
-{
-if(paragraph2.style.display==='none') 
-  paragraph2.style.display = 'block';
- else
-  paragraph2.style.display = 'none';
-}
-
-let paragraph3 = document.querySelector('.unvisible_CBCD_paragraph3')
-console.log(paragraph.style)
-function showParagraph3()
-{
-if(paragraph3.style.display==='none') 
-  paragraph3.style.display = 'block';
- else
-  paragraph3.style.display = 'none';
-}
-let paragraph4 = document.querySelector('.unvisible_CBCD_paragraph4')
-console.log(paragraph.style)
-function showParagraph4()
-{
-if(paragraph4.style.display==='none') 
-  paragraph4.style.display = 'block';
- else
-  paragraph4.style.display = 'none';
+ P_On.style.display = 'none';
 }
