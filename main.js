ca8eb12cabc6e72f9f200b513634f493fcdf847b
MatveyCoding <korchemanov56@mail.ru>
2023-11-27 14:26:56 +0400

    Финальная версия

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-function showParagraphP(P)
+function showParagraphP(P,event)
 {
+  event.preventDefault();
   let P_On = document.querySelector(P)
  if(P_On.style.display==='none') 
  P_On.style.display = 'block';
  else
  P_On.style.display = 'none';
+ 
 }
